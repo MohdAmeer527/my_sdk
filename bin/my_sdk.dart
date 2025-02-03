a9c2145760f5ec2c2b2708b645971911bd412c28
MohdAmeer527 <mohdameer527@gmail.com>
2025-02-03 11:21:52 +0530

    license add

--- bin/my_sdk.dart
+++ bin/my_sdk.dart
@@ -1,5 +1,5 @@
 import 'package:my_sdk/my_sdk.dart' as my_sdk;
 
 void main(List<String> arguments) {
-  print('Hello world: ${my_sdk.calculate()}!');
+  print('Hello world: ${my_sdk.StringUtils.reverse('Hello world')}!');
 }
